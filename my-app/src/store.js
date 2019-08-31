b34c97b29394801e71a481629018098a61dafe44
edwardtyrant <ruanyixuan1112@gmail.com>
2019-08-31 21:26:29 +0800

    change book and movie show page

--- my-app/src/store.js
+++ my-app/src/store.js
@@ -25,6 +25,8 @@ export default new Vuex.Store({
     introduction: '',
     selfComments: [],
     searchBookId: '5d61fe1359f0004904de51b6',
+    book_img_prefix: 'http://114.115.151.96:8666/PosterPicture/MovieAccount/',
+    movie_img_prefix: 'http://114.115.151.96:8666/PosterPicture/MovieAccount/',
     groupStatus: {
       joinedNum: 1,
       myGroups:[{
@@ -517,6 +519,9 @@ export default new Vuex.Store({
     changeCurrentId (state, newVal) {
       state.currentId = newVal
     },
+    changeCurrentBookId (state, newVal) {
+      state.currentBookId = newVal
+    },
     changeJoinState (state) {
       state.joined = !state.joined;
     },
@@ -539,6 +544,9 @@ export default new Vuex.Store({
     },
     changeCurrentBMId (context, newVal) {
       context.commit('changeCurrentId', newVal)
+    },
+    changeCurrentBookId (context, newVal) {
+      context.commit('changeCurrentBookId', newVal)
     },
     getTodayRecommend (context, newList) {
       context.commit('changeTodayRecommendContent', newList)
